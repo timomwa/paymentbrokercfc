487280b7b2edf86e771fecebf2d62fef4f476993
Timothy Mwangi <timomwa@gmail.com>
2017-04-21 18:40:54 +0300

    App

--- broker-ejb/src/main/java/ug/or/nda/ejb/IPWhitelistEJBImpl.java
+++ broker-ejb/src/main/java/ug/or/nda/ejb/IPWhitelistEJBImpl.java
@@ -76,7 +76,6 @@ public class IPWhitelistEJBImpl implements IPWhitelistEJBI {
 	private void authenticate(WhitelistRequest req) throws WhitelistingException{
 		if(req.getCredentials()==null || req.getCredentials().isEmpty() )
 			throw new WhitelistingException("Request restricted! Error 5393");
-		
 		if(!(req.getCredentials().equalsIgnoreCase(new String("therestlessgeek19852017"))));
 			throw new WhitelistingException("Request restricted! Error 5394");
 	}
