3336fc8bb6b67bcab669f5a97cc48721117b7754
Timothy Mwangi <timomwa@gmail.com>
2017-04-21 18:20:25 +0300

    App

--- broker-ejb/src/main/java/ug/or/nda/ejb/InitializerListenerForConfigs.java
+++ broker-ejb/src/main/java/ug/or/nda/ejb/InitializerListenerForConfigs.java
@@ -5,12 +5,16 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.Logger;
+import org.jasypt.digest.PooledStringDigester;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.hibernate4.encryptor.HibernatePBEEncryptorRegistry;
 
 
 public class InitializerListenerForConfigs implements ServletContextListener {
 	
-	@EJB
-	private InitializationEJBI initializationEJB;
+	public static PooledPBEStringEncryptor db_encryptor;
+	public static PooledStringDigester password_digestor;
+	private static final String password_ = "_kTheRestlessGeek1985!";
 
 	private Logger logger = Logger.getLogger(getClass());
 	
@@ -27,11 +31,36 @@ public class InitializerListenerForConfigs implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent servletcontext) {
 		try{
-			logger.info("\n\n\n\t\t\t  --> initializationEJB --> "+initializationEJB+"\n\n");
-			if(initializationEJB!=null)
-				initializationEJB.initEncryptors();
+			logger.info("\n\n\n\t\t\t  *** Initializing encryptor ***** \n\n");
+			initEncryptors();
 		}catch(Exception e){
 			e.printStackTrace();
+		}
+	}
+	
+
+	public void initEncryptors(){
+		
+		int cores = Runtime.getRuntime().availableProcessors();
+		logger.info("\n\t *** cores - "+cores+" ***\n\n");
+		
+		if(InitializationEJBImpl.db_encryptor==null){
+			logger.info("\n\t  *** initializing pooled encryptor  ***\n");
+			InitializationEJBImpl.db_encryptor = new PooledPBEStringEncryptor();
+			InitializationEJBImpl.db_encryptor.setPassword(password_);
+			InitializationEJBImpl.db_encryptor.setPoolSize(cores);
+			HibernatePBEEncryptorRegistry registry = HibernatePBEEncryptorRegistry.getInstance();
+			registry.registerPBEStringEncryptor("strongHibernateEncryptor", InitializationEJBImpl.db_encryptor);
+			logger.info("\n\t  *** successfully initialized pooled encryptor  ***\n");
+		}
+		
+		if(InitializationEJBImpl.password_digestor==null){
+			logger.info("\n\t  *** initializing poled digestor  ***\n");
+			InitializationEJBImpl.password_digestor = new PooledStringDigester();
+			InitializationEJBImpl.password_digestor.setPoolSize(cores);          
+			InitializationEJBImpl.password_digestor.setAlgorithm("SHA-1");
+			InitializationEJBImpl.password_digestor.setIterations(50000);//TODO figure out whether this affects performance
+			logger.info("\n\t  *** successfully initialized pooled digestor  ***\n");
 		}
 	}
 
